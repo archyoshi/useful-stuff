8962eab17f4cec560b794b6d65b5cb5261e42453
Thami Inaflas <inaflas.thami@gmail.com>
2015-12-09 16:49:26 +0100

    new methods related to JSON

--- JavaDev/src/TheProgram.java
+++ JavaDev/src/TheProgram.java
@@ -1,3 +1,8 @@
+import java.util.Arrays;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class TheProgram {
 	
@@ -8,7 +13,74 @@ public class TheProgram {
 	public static void main(String[] args){
 		System.out.println("This is supposed to be a program for calling / testing other tools.");
 		
-		DownloadFile.downloadFile(filePath, fileName, urlString);
+		int testNumber = 3;
+		/*
+		 * 1: testing download file.
+		 * 2: testing if a string of a string array contains a value
+		 * 3: testing to transform a json like string to a json string
+		 */
+		
+		
+		switch (testNumber) {
+		
+		case 1:
+			DownloadFile.downloadFile(filePath, fileName, urlString);
+			break;
+			
+		case 2:
+			String [] table = {"abc", "bcd", "cde"};
+			String v1 = "bc";
+			String v2 = "ac";
+			String v3 = "bcd";
+			System.out.println("Table = "+ Arrays.toString(table));
+			System.out.println("Result for "+ v1 +" = "+ TheTools.doesTContain(table, v1));
+			System.out.println("Result for "+ v2 +" = "+ TheTools.doesTContain(table, v2));
+			
+			System.out.println("Now trying Arrays.asList contains (not exactly what I want)\n but could be useful with v3 !");
+			System.out.println("Result for "+ v1 +" = "+ Arrays.asList(table).contains(v1));
+			System.out.println("Result for "+ v2 +" = "+ Arrays.asList(table).contains(v2));
+			System.out.println("Result for "+ v3 +" = "+ Arrays.asList(table).contains(v3));
+			
+			break;
+			
+		case 3:
+			String jls = "{Global={label=Global, value=0@Global/, isSelected=true, excluded=null, count=325576, hasChildren=true, "
+					+ "isParent=false}, Local={label=Local, value=[fiest=0@Local/, isSelected=false], excluded=null, count=3265,"
+					+ " hasChildren=true, isParent=false}}";
+			
+			String jls1 = "{label=Sources, values={Global={label=Global, value=0@Global/, isSelected=false, "
+					+ "excluded=null, count=325576, hasChildren=true, isParent=false}, Local={label=Local, value=0@Local/, "
+					+ "isSelected=false, excluded=null, count=3265, hasChildren=true, isParent=false}}, "
+					+ "hasMoreValues=false, rendering=null}";
+			
+			String jls2 = "{label=Sources,?values={Global={label=Global,?value=0@Global/,?isSelected=false,?excluded=null,?count=325576,?hasChildren=true,?isParent=false},?Local={label=Local,?value=0@Local/,?isSelected=false,?excluded=null,?count=3265,?hasChildren=true,?isParent=false}},?hasMoreValues=false,?rendering=null}";
+			System.out.println("JsonLikeString =\n" + jls + "\n");
+			
+			System.out.println("result of JLS as a well formed json String =\n" + TheTools.jsonifyAString(jls, "=", ",", "\\{", "\\}", "\\[", "\\]"));
+			
+			System.out.println("result of JLS1 as a well formed json String =\n" + TheTools.jsonifyAString(jls1, "=", ",", "\\{", "\\}", "\\[", "\\]"));
+			
+			System.out.println("result of JLS2 as a well formed json String =\n" + TheTools.jsonifyAString(jls2, "=", ",", "\\{", "\\}", "\\[", "\\]"));
+			
+			JSONObject jsonObj = null;
+			
+			try {
+				jsonObj = new JSONObject(TheTools.jsonifyAString(jls2, "=", ",", "\\{", "\\}", "\\[", "\\]"));
+				System.out.println(jsonObj.getJSONObject("values").getJSONObject("Global").getString("isSelected"));
+				
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+
+			
+			break;
+		default:
+			System.out.println("No test chosen, bye !");
+			break;
+		}
+		
+		
+		
 		
 	}
 }
--- JavaDev/src/TheTools.java
+++ JavaDev/src/TheTools.java
@@ -1,3 +1,78 @@
+//import org.json.JSONObject;
+
 public class TheTools {
+	
+	public static boolean doesTContain(String [] T, String value){
+		boolean result = false;
+			if(null!=T){
+				for(String s : T){
+					if(s.contains(value)){
+						result=true;
+					}
+				}
+			}
+		return result;
+	}
+	
+	public static String jsonifyAStringWithoutArrays(String jsonLikeString, String assignmentOp, String separator, String objectOpeningDelimiter, String objectClosingDelimiter, String arrayOpeningDelimiter, String arrayClosingDelimiter){
+		
+		String jsonString = jsonLikeString;
+		
+		
+		jsonString = jsonString.replaceAll(assignmentOp, "\":\"");
+		jsonString = jsonString.replaceAll("\""+objectOpeningDelimiter, "{");
+		jsonString = jsonString.replaceAll("\""+arrayOpeningDelimiter, "[");
+		jsonString = jsonString.replaceAll(objectOpeningDelimiter, "{\"");
+		jsonString = jsonString.replaceAll(objectClosingDelimiter, "\"}");
+		jsonString = jsonString.replaceAll(separator+" ", ", \"");
+		jsonString = jsonString.replaceAll(arrayOpeningDelimiter, "[\"");
+		jsonString = jsonString.replaceAll(arrayClosingDelimiter, "\"]");
+		jsonString = jsonString.replaceAll(separator, "\",");
+		jsonString = jsonString.replaceAll(objectClosingDelimiter+"\"", "}");
+		jsonString = jsonString.replaceAll(arrayClosingDelimiter+"\"", "]");
+		jsonString = jsonString.replaceAll(assignmentOp, "\":");
+		
+		
+		return jsonString;
+	}
+	
+	public static String jsonifyAString(String jsonLikeString, String assignmentOp, String separator, String objectOpeningDelimiter, String objectClosingDelimiter, String arrayOpeningDelimiter, String arrayClosingDelimiter){
+		
+		String jsonString = jsonLikeString;
+		
+		String finalJsonString = "";
+		String temp = "";
+		
+		jsonString = jsonString.replaceAll(assignmentOp, "\":\"");
+		jsonString = jsonString.replaceAll("\""+objectOpeningDelimiter, "{");
+		jsonString = jsonString.replaceAll("\""+arrayOpeningDelimiter, "[");
+		jsonString = jsonString.replaceAll(objectOpeningDelimiter, "{\"");
+		jsonString = jsonString.replaceAll(objectClosingDelimiter, "\"}");
+		jsonString = jsonString.replaceAll(separator, ", \"");
+//		jsonString = jsonString.replaceAll(separator+" ", ", \"");
+		jsonString = jsonString.replaceAll(arrayOpeningDelimiter, "[\"");
+		jsonString = jsonString.replaceAll(arrayClosingDelimiter, "\"]");
+		jsonString = jsonString.replaceAll(separator, "\",");
+		jsonString = jsonString.replaceAll(objectClosingDelimiter+"\"", "}");
+		jsonString = jsonString.replaceAll(arrayClosingDelimiter+"\"", "]");
+		jsonString = jsonString.replaceAll(assignmentOp, "\":");
+		
+		finalJsonString = jsonString;
+		
+		while(jsonString.indexOf("[") != -1){
+			finalJsonString = jsonString.substring(0, jsonString.indexOf("["));
+			jsonString = jsonString.substring(jsonString.indexOf("["));
+			temp = jsonString.substring(0, jsonString.indexOf("]") + 1);
+			temp = temp.replaceAll("\\[", "[{");
+			temp = temp.replaceAll("\\]", "}]");
+			temp = temp.replaceAll(",", "}, {");
+			temp = temp.replaceAll("\\{ \"", "{\"");
+			finalJsonString += temp;
+			finalJsonString += jsonString.substring(jsonString.indexOf("]") + 1);
+			jsonString = jsonString.substring(jsonString.indexOf("]"));
+		}
+		
+		return finalJsonString;
+	}
 
 }
